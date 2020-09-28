6bb2d7f14c7ed492655b2425d0cec88042dc6fd5
sawu2019 <jennings.sawu@etu.unilim.fr>
2020-09-28 13:06:16 +0100

    Arrangement home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,19 +21,9 @@
        folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{asset('dist/css/skins/_all-skins.min.css')}}">
 
-    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css')}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css')}}">
+
     <!-- DataTables -->
     <link rel="stylesheet" href="{{asset('plugins/datatables/dataTables.bootstrap.css')}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('dist/css/AdminLTE.min.css')}}">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{asset('dist/css/skins/_all-skins.min.css')}}">
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -487,9 +477,7 @@
 
                 </div>
                 <!-- /.tab-pane -->
-                <!-- Stats tab content -->
-                <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-                <!-- /.tab-pane -->
+
                 <!-- Settings tab content -->
                 <div class="tab-pane" id="control-sidebar-settings-tab">
                     <form method="post">
@@ -585,22 +573,12 @@
     <script src="{{asset('plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
     <!-- ChartJS 1.0.1 -->
     <script src="{{asset('plugins/chartjs/Chart.min.js')}}"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{asset('dist/js/pages/dashboard2.js')}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{asset('dist/js/demo.js')}}"></script>
 
-    <!-- jQuery 2.2.3 -->
-    <script src="{{asset('plugins/jQuery/jquery-2.2.3.min.js')}}"></script>
-    <!-- Bootstrap 3.3.6 -->
-    <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+    
+    
     <!-- DataTables -->
     <script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('plugins/datatables/dataTables.bootstrap.min.js')}}"></script>
-    <!-- SlimScroll -->
-    <script src="{{asset('plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
-    <!-- FastClick -->
-    <script src="{{asset('plugins/fastclick/fastclick.js')}}"></script>
     <script>
         $(function() {
             $("#example1").DataTable();
@@ -616,4 +594,5 @@
     </script>
 
 </body>
+
 </html>
